62716ed4492eb867e6e205e9ff97a0d989c9ac40
Francilândio <58220640+Francilandio07@users.noreply.github.com>
2024-01-18 21:07:57 -0300

    Resolvendo o desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,37 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
+    var inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        for(individuo in usuario){
+            inscritos.add(individuo)
+        }
+        
+        println("Usuários Inscritos na formação $nome: ${inscritos}")
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+fun main() {    
+    //Criando conteúdo
+    val conteudo1 = ConteudoEducacional("Matemática", 20)
+    val conteudo2 = ConteudoEducacional("Gramática", 40)
+    
+    val listadeconteudos = listOf(conteudo1, conteudo2)
+    
+    //Criando Formação
+    val formacao = Formacao("Conteudo geral", listadeconteudos, Nivel.INTERMEDIARIO)
+    
+    //Criando Usuário
+    val usuario1 = Usuario("Francilandio", 22)
+    val usuario2 = Usuario("Antonia", 19)
+    
+    //Matriculando o usuário
+    formacao.matricular(usuario1, usuario2)
 }
